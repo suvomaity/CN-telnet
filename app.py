356d2aeffa5cd76e19bbc680792de3fe457b654c
Suvo <109958974+suvomaity@users.noreply.github.com>
2025-11-15 02:15:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -162,5 +162,7 @@ def handle_message(data):
     room = data['room']
     emit('message', data, room=room)
 
-if __name__ == '__main__':
-    socketio.run(app, debug=True, port=5000)
+if __name__ == "__main__":
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    socketio.run(app, host="0.0.0.0", port=port)
